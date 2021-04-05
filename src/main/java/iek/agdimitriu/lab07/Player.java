55872782763da9f3c7e74d21696d3bb7fc87d0d6
sp <sp@AccessCloud>
2021-04-05 17:41:03 +0300

    Player Constructor

--- src/main/java/iek/agdimitriu/lab07/Player.java
+++ src/main/java/iek/agdimitriu/lab07/Player.java
@@ -5,10 +5,20 @@
  */
 package iek.agdimitriu.lab07;
 
+import java.util.Date;
+
 /**
  *
  * @author ovidi
  */
 public class Player {
+   
+    private String firstName; //Ονομα παικτη
+    private String lastName; //Επιθετο Παικτη
+    private Date birthDate; //Ημερομηνια Γεννησης Παικτη
     
+    public Player (String f, String 1){
+        this.firstName=f;
+        this.lastName=1;
+    }
 }
